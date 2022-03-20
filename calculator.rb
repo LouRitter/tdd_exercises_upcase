691fd1d8304cf3c318deb7086ac5bc4ddbb86713
louritter <louritter1@gmail.com>
2022-03-19 22:44:42 -0400

    created a test driven method for factorials following along video 2 in series

--- calculator.rb
+++ calculator.rb
@@ -7,8 +7,21 @@ class Calculator
     a+b
   end
 
+  def factorial(n)
+    # if n < 1 
+    #   return 1
+    # else
+    #   n * factorial(n-1)
+    # end
+    if n == 0 
+      1
+    else
+      (1..n).reduce(:*) 
+    end
+  end
 
 end
+
 describe Calculator do 
   describe "#add" do
     it "returns the sum of its arguments" do
@@ -19,6 +32,18 @@ describe Calculator do
     it "returns the sum of two different arguments" do
       calc = Calculator.new
       expect(calc.add(3,4)).to eq(7)
+    end
+  end
+
+  describe "#factorial" do 
+    it "returns 1 when given 0" do 
+      calc = Calculator.new
+      expect(calc.factorial(0)).to eq(1)
+    end
+
+    it "returns 120 when given 5" do 
+      calc = Calculator.new
+      expect(calc.factorial(5)).to eq(120)
     end
   end
 end
