6501957d075f072d38764919271309a22ac1b09f
louritter <louritter1@gmail.com>
2022-03-20 21:22:02 -0400

    refactored tests

--- unit_converter.rb
+++ unit_converter.rb
@@ -21,16 +21,30 @@ class UnitConverter
   private 
 
   CONVERSION_FACTORS = {
-    cup:{
-      liter: 0.236588
+    liter:{
+      cup: 4.22675,
+      pint: 2.11338, 
+      liter: 1,  
+    },
+    gram: {
+      kilogram: 0.001, 
+      gram: 1
     }
   }
 
   def conversion_factor(from:, to:)
-    CONVERSION_FACTORS[from][to] || 
+    dimension = common_dimension(from, to) ||
       raise(DimensionalMismatchError, "Cant convert from #{from} to #{to}")
+    CONVERSION_FACTORS[dimension][to] / CONVERSION_FACTORS[dimension][from]
+
   end
 
+  def common_dimension(from, to)
+    CONVERSION_FACTORS.keys.find do |canonical_unit|
+      CONVERSION_FACTORS[canonical_unit].keys.include?(from) &&
+      CONVERSION_FACTORS[canonical_unit].keys.include?(to)
+    end
+  end
 
 end
 
@@ -45,10 +59,27 @@ describe UnitConverter do
       expect(result.unit).to eq(:liter)
     end
 
+    it "translates between 2 units of the same dimension (gram & kg)" do
+      kg = Quantity.new(2, :kilogram) 
+      converter = UnitConverter.new(kg, :gram)
+
+      result = converter.convert
+      expect(result.amount).to be_within(0.001).of(2000)
+      expect(result.unit).to eq(:gram)
+    end
+
     it "raises an error if the two units are not of same dimension" do 
       cups = Quantity.new(2, :cup)
       converter = UnitConverter.new(cups, :gram)
       expect{converter.convert}.to raise_error(DimensionalMismatchError)
+    end
+
+    it "returns itself when given 2 of the same unit types" do 
+      cups = Quantity.new(2, :cup)
+      converter = UnitConverter.new(cups, :cup)
+      result = converter.convert
+      expect(result.amount).to be_within(0.001).of(2)
+      expect(result.unit).to eq(:cup)
     end
   end
 end
